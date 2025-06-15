ba7ddf5373fddf5815992086d1363a6e9c1bd6be
gkishanrao <gkishanrao@gmail.com>
2025-06-15 13:11:28 -0700

    update code changes

---
+++ src/test/java/com/nirwantech/stock_trading_server/StockTradingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nirwantech.stock_trading_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockTradingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
